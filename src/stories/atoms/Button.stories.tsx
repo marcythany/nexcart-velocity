e314835381eca94da242d5a042bd71b87fb12ef0
Marcy Thany <marcythany@gmail.com>
2025-09-11 11:41:23 -0300

    deleted:    bun.lock 	new file:   package-lock.json 	modified:   package.json 	modified:   src/stories/Page.tsx 	modified:   src/stories/atoms/Button.stories.tsx 	modified:   vitest.config.ts

--- src/stories/atoms/Button.stories.tsx
+++ src/stories/atoms/Button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/nextjs';
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { Plus, ShoppingCart } from 'lucide-react';
 import Button from '../../components/atoms/Button';
 
